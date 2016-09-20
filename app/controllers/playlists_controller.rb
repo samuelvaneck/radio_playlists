3798e62719e3f3b383935ec128bd6b613fd7312a
Samuel van Eck <samuelvaneck@gmail.com>
2016-09-20 21:11:27 +0200

    changed the styling of the web page

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,11 +1,7 @@
 class PlaylistsController < ApplicationController
 
   def index
-    @playlists = Playlist.all
-  end
-
-  def show
-    @playlist = Playlist.find(params[:id])
+    @playlists = Playlist.order(counter: :desc)
   end
 
 end
