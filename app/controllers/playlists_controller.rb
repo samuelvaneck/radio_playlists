0742687068bedd9d94626e6abdbd166118459901
Samuel van Eck <samuelvaneck@gmail.com>
2016-09-28 19:28:05 +0200

    added simple search function

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -3,7 +3,11 @@ class PlaylistsController < ApplicationController
 
 
   def index
-    @playlists = Playlist.order(total_counter: :desc).paginate(page: params[:page]).per_page(25)
+    if params[:search].present?
+      @playlists = Playlist.where("fullname ILIKE ?", "%#{params[:search]}%").paginate(page: params[:page]).per_page(25)
+    else
+      @playlists = Playlist.order(total_counter: :desc).paginate(page: params[:page]).per_page(25)
+    end
   end
 
 end
