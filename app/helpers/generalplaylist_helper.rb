5555fc9cd491666be47e195f59ae5011ed16dfce
Samuel van Eck <samuelvaneck@gmail.com>
2016-11-25 13:19:15 +0100

    changed view to not use helper methode

--- app/helpers/generalplaylist_helper.rb
+++ app/helpers/generalplaylist_helper.rb
@@ -1,12 +1,3 @@
 module GeneralplaylistHelper
 
-  def get_artist_name(artist_id)
-    if artist_id != nil
-      name = Artist.find(artist_id).name
-      return name
-    else
-      return false
-    end
-  end
-
 end
