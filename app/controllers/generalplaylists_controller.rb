8b72191d842c470d32949374ed13e9599dfd71e3
Samuel van Eck <samuelvaneck@gmail.com>
2017-04-16 21:43:58 +0200

    added search filter option and transparent backgroud

--- app/controllers/generalplaylists_controller.rb
+++ app/controllers/generalplaylists_controller.rb
@@ -2,16 +2,20 @@ class GeneralplaylistsController < ApplicationController
 
   def index
     if params[:search_top_song].present?
-      @top_songs = Song.where("title ILIKE ?", "%#{params[:search_top_song]}%").limit(3)
+      @top_songs = Song.joins(:artist).where("songs.title ILIKE ? OR artists.name ILIKE ?", "%#{params[:search_top_song]}%", "%#{params[:search_top_song]}%").limit(5)
     else
-      @top_songs = Song.order(week_counter: :DESC).limit(3)
+      @top_songs = Song.order(week_counter: :DESC).limit(5)
     end
+    @top_songs.order!(week_counter: :DESC)
+    @top_songs = @top_songs.reorder!("#{params[:set_counter_top_songs]} DESC") if params[:set_counter_top_songs]
 
     if params[:search_top_artist].present?
-      @top_artists = Artist.where("name ILIKE ?", "%#{params[:search_top_artist]}%").limit(3)
+      @top_artists = Artist.where("name ILIKE ?", "%#{params[:search_top_artist]}%").limit(5)
     else
-      @top_artists = Artist.order(week_counter: :DESC).limit(3)
+      @top_artists = Artist.order(week_counter: :DESC).limit(5)
     end
+    @top_artists.order!(week_counter: :DESC)
+    @top_artists.reorder!("#{params[:set_counter_top_artists]} DESC") if params[:set_counter_top_artists]
 
     @target = params[:target]
   end
