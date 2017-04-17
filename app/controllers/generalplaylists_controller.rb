c2f46a9d2cdb07d22b1ca1412d1dba6d1655820a
Samuel van Eck <samuelvaneck@gmail.com>
2017-04-17 10:37:40 +0200

    added navigation buttons and fixed issue with select box

--- app/controllers/generalplaylists_controller.rb
+++ app/controllers/generalplaylists_controller.rb
@@ -20,35 +20,35 @@ class GeneralplaylistsController < ApplicationController
     @target = params[:target]
   end
 
-  def song_details
-    @details = Song.where("fullname ILIKE ?", "%#{params[:search_fullname]}%")
-  end
-
-  def today_played_songs
-    @today_played_songs = Generalplaylist.today_played_songs.paginate(page: params[:page]).per_page(10)
-  end
-
-  def top_songs
-    if params[:page].present?
-      @counter = (params[:page].to_i * 10) -10
-    else
-      @counter = 0
-    end
-    @top_songs = Generalplaylist.top_songs.paginate(page: params[:page]).per_page(10)
-  end
-
-  def top_artists
-    if params[:page].present?
-      @counter = (params[:page].to_i * 10) -10
-    else
-      @counter = 0
-    end
-    @top_artists = Generalplaylist.top_artists.paginate(page: params[:page]).per_page(10)
-  end
-
-  def autocomplete
-    @results = Song.order(:fullname).where("fullname ILIKE ?", "%#{params[:term]}%").limit(10)
-    render json: @results.map(&:fullname)
-  end
+  # def song_details
+  #   @details = Song.where("fullname ILIKE ?", "%#{params[:search_fullname]}%")
+  # end
+  #
+  # def today_played_songs
+  #   @today_played_songs = Generalplaylist.today_played_songs.paginate(page: params[:page]).per_page(10)
+  # end
+  #
+  # def top_songs
+  #   if params[:page].present?
+  #     @counter = (params[:page].to_i * 10) -10
+  #   else
+  #     @counter = 0
+  #   end
+  #   @top_songs = Generalplaylist.top_songs.paginate(page: params[:page]).per_page(10)
+  # end
+  #
+  # def top_artists
+  #   if params[:page].present?
+  #     @counter = (params[:page].to_i * 10) -10
+  #   else
+  #     @counter = 0
+  #   end
+  #   @top_artists = Generalplaylist.top_artists.paginate(page: params[:page]).per_page(10)
+  # end
+  #
+  # def autocomplete
+  #   @results = Song.order(:fullname).where("fullname ILIKE ?", "%#{params[:term]}%").limit(10)
+  #   render json: @results.map(&:fullname)
+  # end
 
 end
