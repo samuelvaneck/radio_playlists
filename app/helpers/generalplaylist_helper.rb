877c9cf03a0f164339bff602dc8e06779ff257fd
Samuel van Eck <samuelvaneck@gmail.com>
2017-06-28 19:19:03 +0200

    added random album image for artists

--- app/helpers/generalplaylist_helper.rb
+++ app/helpers/generalplaylist_helper.rb
@@ -1,3 +1,12 @@
 module GeneralplaylistHelper
 
+  def random_album_image(top_artist)
+    image = nil
+    if top_artist.songs.map {|song| song.artwork_url}.any?
+      while image == nil do
+        image = top_artist.songs.map { |song| song.artwork_url }.sample
+      end
+    end
+    return image
+  end
 end
