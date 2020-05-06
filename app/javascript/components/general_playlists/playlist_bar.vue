9196a31b90ad4c2acd954baafb9672bf4bf39b41
Samuel van Eck <samuel.van.eck@simbeyond.com>
2020-05-06 18:25:36 +0200

    Added search filter general playlists

--- app/javascript/components/general_playlists/playlist_bar.vue
+++ app/javascript/components/general_playlists/playlist_bar.vue
@@ -1,19 +1,85 @@
 <template>
-  <div class='d-flex flex-row'>
-    <PlaylistGroup />
+  <div class='d-flex flex-column'>
+    <div class='d-flex flex-row'>
+      <h3>Playlists</h3>
+      <div class='ml-auto'>
+        <SearchBar @search='onKeyUpSearch' @filter='onRadioStationSelect' />
+      </div>
+    </div>
+    <PlaylistGroup v-bind:items='items' @scroll='onScroll' />
   </div>
 </template>
 
 <script>
+  import SearchBar from '../application/search_bar.vue'
   import NavArrow from '../application/slider_button.vue'
   import PlaylistGroup from './playlist_group.vue'
 
   export default {
     data() {
       return {
-        term: ''
+        term: '',
+        items: [],
+        page: 1,
+        requestInProgress: false,
+        lastPage: false,
+        radioStationFilter: '',
       }
     },
-    components: { NavArrow, PlaylistGroup }
+    methods: {
+      getItems: function(append = false) {
+        const url = '/generalplaylists?radiostation_id=' + this.radioStationFilter + '&search_term=' + this.term + '&page=' + this.page
+        console.log({ append, url })
+        const options = {
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json;charset=UTF-8'
+          }
+        }
+
+        if (!append) this.items = []
+        this.requestInProgress = true
+
+        fetch(url, options).then(res => res.json())
+          .then(d => {
+            // append if scroll else set new items
+            this.items = append ? this.items.concat(d.data) : d.data
+            this.requestInProgress = false
+            // dont make new request if there are no more entries
+            this.lastPage = d.data.length < 10
+          })
+      },
+      onKeyUpSearch(value) {
+        if (this.timer) {
+            clearTimeout(this.timer);
+            this.timer = null;
+        }
+        this.timer = setTimeout(() => {
+          this.term = value
+          this.page = 1
+          this.lastPage = false
+          this.getItems()
+        }, 400);
+      },
+      onScroll(row) {
+        const rightEdgeOfRow = (row.scrollLeft + row.offsetWidth) >= (row.scrollWidth - 50)
+        if (!rightEdgeOfRow || this.requestInProgress || this.lastPage) return
+        
+        this.page++
+        this.getItems(true)
+      },
+      onRadioStationSelect(value) {
+        this.radioStationFilter = value
+        this.page = 1
+        this.lastPage = false
+        this.getItems()
+      }
+    },
+    created() {
+      const url = '/generalplaylists'
+      this.getItems(url);
+    },
+    components: { SearchBar, NavArrow, PlaylistGroup }
   }
 </script>
--- app/javascript/components/general_playlists/playlist_group.vue
+++ app/javascript/components/general_playlists/playlist_group.vue
@@ -9,50 +9,13 @@
 <script>
   import Playlist from './playlist.vue'
   export default {
-    data() {
-      return {
-        items: [],
-        page: 1,
-        requestInProgress: false
-      }
-    },
+    props: ['items'],
     components: { Playlist },
     methods: {
       handleScroll(event) {
         const row = event.target
-        const rightEdgeOfRow = (row.scrollLeft + row.offsetWidth) >= (row.scrollWidth - 50)
-        if (!rightEdgeOfRow || this.requestInProgress) return
-        
-        this.page++
-        this.requestInProgress = true
-
-        const url = '/generalplaylists?page=' + this.page
-        const options = {
-          method: 'GET',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json;charset=UTF-8'
-          },
-        }
-
-        fetch(url, options).then(response => response.json())
-          .then(d => {
-            this.items = this.items.concat(d.data)
-            this.requestInProgress = false
-          })
+        this.$emit('scroll', row)
       }
-    },
-    created() {
-      const url = '/generalplaylists'
-      const options = {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json;charset=UTF-8'
-        }
-      }
-      fetch(url, options).then(res => res.json())
-        .then(d => this.items = d.data)
     },
     mounted() {
       this.$el.addEventListener('scroll', this.handleScroll)
