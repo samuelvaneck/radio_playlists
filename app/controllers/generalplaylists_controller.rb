e84ac3a92734bd4858a79d020080824885a2a376
Samuel van Eck <samuel.van.eck@simbeyond.com>
2020-05-07 17:45:11 +0200

    Code refactoring

--- app/controllers/generalplaylists_controller.rb
+++ app/controllers/generalplaylists_controller.rb
@@ -5,58 +5,13 @@ class GeneralplaylistsController < ApplicationController
 
   def index
     # Playlist search options
-    if params[:search_term].present? && params[:radiostation_id].present? && params[:set_counter_playlists].present?
-      @playlists = Generalplaylist.joins(:artist, :song).where("artists.name ILIKE ? OR songs.fullname ILIKE ?", "%#{params[:search_term]}%", "%#{params[:search_term]}%").where("radiostation_id = ?", "#{params[:radiostation_id]}").limit(params[:set_limit_playlists])
-      set_time_playlists
-
-    elsif params[:search_term].present? && params[:radiostation_id].present?
-      @playlists = Generalplaylist.joins(:artist, :song).where("artists.name ILIKE ? OR songs.fullname ILIKE ?", "%#{params[:search_term]}%", "%#{params[:search_term]}%").where("radiostation_id = ?", "#{params[:radiostation_id]}").limit(params[:set_limit_playlists])
-
-    elsif params[:search_term].present? && params[:set_counter_playlists].present?
-      @playlists = Generalplaylist.joins(:artist, :song).where("artists.name ILIKE ? OR songs.fullname ILIKE ?", "%#{params[:search_term]}%", "%#{params[:search_term]}%").limit(params[:set_limit_playlists])
-      set_time_playlists
-
-    elsif params[:radiostation_id].present? && params[:set_counter_playlists].present?
-      @playlists = Generalplaylist.where('radiostation_id = ?', "#{params[:radiostation_id]}").limit(params[:set_limit_playlists])
-      set_time_playlists
-
-    elsif params[:search_term].present?
-      @playlists = Generalplaylist.joins(:artist, :song).where('artists.name ILIKE ? OR songs.fullname ILIKE ?', "%#{params[:search_term]}%", "%#{params[:search_term]}%").limit(params[:set_limit_playlists])
-
-    elsif params[:radiostation_id].present?
-      @playlists = Generalplaylist.where('radiostation_id = ?', "#{params[:radiostation_id]}").limit(params[:set_limit_playlists])
-
-    elsif params[:set_counter_playlists].present?
-      set_time_playlists
-      @playlists = Generalplaylist.order(created_at: :DESC).limit(params[:set_limit_playlists])
-
-    else
-      @playlists = if params[:set_limit_playlists].present?
-                     Generalplaylist.order(created_at: :DESC).limit(params[:set_limit_playlists])
-                   else
-                     Generalplaylist.order(created_at: :DESC).limit(5)
-                   end
-    end
-
-    @playlists.order!(created_at: :DESC)
+    @playlists = Generalplaylist.order(created_at: :DESC)
+    @playlists = @playlists.joins(:artist, :song)
+                           .where!('artists.name ILIKE ? OR songs.fullname ILIKE ?', "%#{params[:search_term]}%", "%#{params[:search_term]}%") if params[:search_term].present?
+    @playlists = @playlists.where!('radiostation_id = ?', params[:radiostation_id]) if params[:radiostation_id].present?
 
     @playlists = @playlists.paginate(page: params[:page], per_page: 10)
 
     respond_with GeneralplaylistSerializer.new(@playlists).serializable_hash.to_json
-  end
-
-  def set_time_playlists
-    case params[:set_counter_playlists]
-    when 'day'
-      @playlists = @playlists.where!('generalplaylists.created_at > ?', 1.day.ago)
-    when 'week'
-      @playlists = @playlists.where!('generalplaylists.created_at > ?', 1.week.ago)
-    when 'month'
-      @playlists = @playlists.where!('generalplaylists.created_at > ?', 1.month.ago)
-    when 'year'
-      @playlists = @playlists.where!('generalplaylists.created_at > ?', 1.year.ago)
-    when 'total'
-      @playlists
-    end
   end
 end
