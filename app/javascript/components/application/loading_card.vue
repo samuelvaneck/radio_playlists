1534de981c8c3cbed70f2854c0592e4a536dc0f4
Samuel van Eck <samuel.van.eck@simbeyond.com>
2020-05-26 16:41:57 +0200

    Add loading card to playlists

---
+++ app/javascript/components/application/loading_card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='card playlist-card mx-1' style='min-width:190px'>
+    <span class='card-img-top'>
+      <LoadingBar v-bind:height='"190px"' v-bind:width='"90%"' />
+    </span>
+    <div class='card-body'>
+      <div class='d-flex flex-row'>
+        <LoadingBar />
+        <LoadingBar />
+      </div>
+      <div><LoadingBar /></div>
+      <div><LoadingBar /></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import LoadingBar from './loading_bar.vue'
+  export default {
+    components: { LoadingBar }
+  }
+</script>
