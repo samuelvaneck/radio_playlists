75bc3bdb40d1706513ef0cf9e8e10bb54f7a678e
Samuel van Eck <samuel.van.eck@simbeyond.com>
2020-06-07 20:00:15 +0200

    Code refacotring. Add tests

--- app/jobs/send_status_email.rb
+++ app/jobs/send_status_email.rb
@@ -4,20 +4,10 @@ class SendStatusEmail < ApplicationJob
   queue_as :default
 
   def perform
-    results = {
-      radio_1: Radiostation.find_by(name: 'Radio 1').status,
-      radio_2: Radiostation.find_by(name: 'Radio 2').status,
-      radio_3fm: Radiostation.find_by(name: 'Radio 3FM').status,
-      radio_4: Radiostation.find_by(name: 'Radio 4').status,
-      radio_5: Radiostation.find_by(name: 'Radio 5').status,
-      sky_radio: Radiostation.find_by(name: 'Sky Radio').status,
-      radio_veronica: Radiostation.find_by(name: 'Radio Veronica').status,
-      radio_538: Radiostation.find_by(name: 'Radio 538').status,
-      radio_10: Radiostation.find_by(name: 'Radio 10').status,
-      q_music: Radiostation.find_by(name: 'Qmusic').status,
-      sublime_fm: Radiostation.find_by(name: 'Sublime FM').status,
-      groot_nieuw_radio: Radiostation.find_by(name: 'Groot Nieuws Radio').status
-    }
+    results = {}
+    Radiostation.each do |radio_station|
+      results[radio_station.name.to_s] = radio_station.status
+    end
 
     StatusMailer.status_mail('samuelvaneck@gmail.com', results).deliver
   end
--- spec/mailers/status_mailer_spec.rb
+++ spec/mailers/status_mailer_spec.rb
@@ -1,4 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe StatusMailer, type: :mailer do
+  let(:radio_station) { FactoryBot.create :radiostation }
+  let(:playlist) { FactoryBot.create :generalplaylist, :filled, radiostation: radio_station }
+  let(:status_mail) { StatusMailer.status_mail('test@test.com', results) }
+  let(:results) { { "#{radio_station.name}": radio_station.status } }
+
+  before { playlist }
+
+  describe 'status_mail' do
+    it 'renders the headers' do
+      expect(status_mail.subject).to eq 'Status radiostation playlists'
+      expect(status_mail.to).to eq ['test@test.com']
+      expect(status_mail.from).to eq ['status@radioplaylists.samuelvaneck.com']
+    end
+
+    it 'renders the email body' do
+      expect(status_mail.body.encoded).to match radio_station.name.humanize
+    end
+  end
 end
