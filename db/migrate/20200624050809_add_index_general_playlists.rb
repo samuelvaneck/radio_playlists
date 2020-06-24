686431273eafc1719c7ada84781bd33b3e9eb17c
Samuel van Eck <samuel.van.eck@simbeyond.com>
2020-06-24 15:43:07 +0200

    Add sidekiq job for migration records

--- db/migrate/20200624050809_add_index_general_playlists.rb
+++ db/migrate/20200624050809_add_index_general_playlists.rb
@@ -2,18 +2,6 @@ class AddIndexGeneralPlaylists < ActiveRecord::Migration[6.0]
   def up
     add_column :generalplaylists, :broadcast_timestamp, :datetime
 
-    Generalplaylist.find_in_batches do |group|
-      group.each do |playlist|
-        next if playlist.broadcast_timestamp.present?
-
-        begin
-          playlist.update(broadcast_timestamp: Time.parse(playlist.created_at.strftime('%F') + ' ' + playlist.time))
-        rescue StandardError => _e
-          playlist.update(broadcast_timestamp: playlist.created_at)
-        end
-      end
-    end
-
     remove_column :generalplaylists, :time, :string
     add_index :generalplaylists, [:song_id, :radiostation_id, :broadcast_timestamp], unique: true, name: 'playlist_radio_song_time'
   end
