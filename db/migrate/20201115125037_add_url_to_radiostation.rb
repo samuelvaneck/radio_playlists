8504c8d4469fccd4381d44f47b9a743128f26b9e
Samuel Van Eck <samuelvaneck@adcombi.com>
2020-11-15 14:15:10 +0100

    Add URL and processor to Radiostations

---
+++ db/migrate/20201115125037_add_url_to_radiostation.rb
@@ -0,0 +1,48 @@
+class AddUrlToRadiostation < ActiveRecord::Migration[6.0]
+  def change
+    add_column :radiostations, :url, :text
+    add_column :radiostations, :processor, :string
+
+    Radiostation.all.each do |rs|
+      case rs.name
+      when 'Radio 1'
+        rs.url = 'https://www.nporadio1.nl/api/tracks'
+        rs.processor = 'npo_api_processor'
+      when 'Radio 2'
+        rs.url = 'https://www.nporadio2.nl/api/tracks'
+        rs.processor = 'npo_api_processor'
+      when 'Radio 3FM'
+        rs.url = 'https://www.npo3fm.nl/api/tracks'
+        rs.processor = 'npo_api_processor'
+      when 'Radio 5'
+        rs.url = 'https://www.nporadio5.nl/api/tracks'
+        rs.processor = 'npo_api_processor'
+      when 'Sky Radio'
+        rs.url = 'https://graph.talparad.io/?query=%7B%0A%20%20getStation(profile%3A%20%22radio-brand-web%22%2C%20slug%3A%20%22sky-radio%22)%20%7B%0A%20%20%20%20title%0A%20%20%20%20playouts(profile%3A%20%22%22%2C%20limit%3A%2010)%20%7B%0A%20%20%20%20%20%20broadcastDate%0A%20%20%20%20%20%20track%20%7B%0A%20%20%20%20%20%20%20%20id%0A%20%20%20%20%20%20%20%20title%0A%20%20%20%20%20%20%20%20artistName%0A%20%20%20%20%20%20%20%20isrc%0A%20%20%20%20%20%20%20%20images%20%7B%0A%20%20%20%20%20%20%20%20%20%20type%0A%20%20%20%20%20%20%20%20%20%20uri%0A%20%20%20%20%20%20%20%20%20%20__typename%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20__typename%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20__typename%0A%20%20%20%20%7D%0A%20%20%20%20__typename%0A%20%20%7D%0A%7D%0A&variables=%7B%7D'
+        rs.processor = 'talpa_api_processor'
+      when 'Radio Veronice'
+        rs.url = 'https://graph.talparad.io/?query=%7B%0A%20%20getStation(profile%3A%20%22radio-brand-web%22%2C%20slug%3A%20%22radio-veronica%22)%20%7B%0A%20%20%20%20title%0A%20%20%20%20playouts(profile%3A%20%22%22%2C%20limit%3A%2010)%20%7B%0A%20%20%20%20%20%20broadcastDate%0A%20%20%20%20%20%20track%20%7B%0A%20%20%20%20%20%20%20%20id%0A%20%20%20%20%20%20%20%20title%0A%20%20%20%20%20%20%20%20artistName%0A%20%20%20%20%20%20%20%20isrc%0A%20%20%20%20%20%20%20%20images%20%7B%0A%20%20%20%20%20%20%20%20%20%20type%0A%20%20%20%20%20%20%20%20%20%20uri%0A%20%20%20%20%20%20%20%20%20%20__typename%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20__typename%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20__typename%0A%20%20%20%20%7D%0A%20%20%20%20__typename%0A%20%20%7D%0A%7D%0A&variables=%7B%7D'
+        rs.processor = 'talpa_api_processor'
+      when 'Radio 538'
+        rs.url = 'https://graph.talparad.io/?query=%7B%0A%20%20getStation(profile%3A%20%22radio-brand-web%22%2C%20slug%3A%20%22radio-538%22)%20%7B%0A%20%20%20%20title%0A%20%20%20%20playouts(profile%3A%20%22%22%2C%20limit%3A%2010)%20%7B%0A%20%20%20%20%20%20broadcastDate%0A%20%20%20%20%20%20track%20%7B%0A%20%20%20%20%20%20%20%20id%0A%20%20%20%20%20%20%20%20title%0A%20%20%20%20%20%20%20%20artistName%0A%20%20%20%20%20%20%20%20isrc%0A%20%20%20%20%20%20%20%20images%20%7B%0A%20%20%20%20%20%20%20%20%20%20type%0A%20%20%20%20%20%20%20%20%20%20uri%0A%20%20%20%20%20%20%20%20%20%20__typename%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20__typename%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20__typename%0A%20%20%20%20%7D%0A%20%20%20%20__typename%0A%20%20%7D%0A%7D%0A&variables=%7B%7D'
+        rs.processor = 'talpa_api_processor'
+      when 'Radio 10'
+        rs.url = 'https://graph.talparad.io/?query=%7B%0A%20%20getStation(profile%3A%20%22radio-brand-web%22%2C%20slug%3A%20%22radio-10%22)%20%7B%0A%20%20%20%20title%0A%20%20%20%20playouts(profile%3A%20%22%22%2C%20limit%3A%2010)%20%7B%0A%20%20%20%20%20%20broadcastDate%0A%20%20%20%20%20%20track%20%7B%0A%20%20%20%20%20%20%20%20id%0A%20%20%20%20%20%20%20%20title%0A%20%20%20%20%20%20%20%20artistName%0A%20%20%20%20%20%20%20%20isrc%0A%20%20%20%20%20%20%20%20images%20%7B%0A%20%20%20%20%20%20%20%20%20%20type%0A%20%20%20%20%20%20%20%20%20%20uri%0A%20%20%20%20%20%20%20%20%20%20__typename%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20__typename%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20__typename%0A%20%20%20%20%7D%0A%20%20%20%20__typename%0A%20%20%7D%0A%7D%0A&variables=%7B%7D'
+        rs.processor = 'talpa_api_processor'
+      when 'Qmusic'
+        rs.url = 'https://api.qmusic.nl/2.4/tracks/plays?limit=1&next=true'
+        rs.processor = 'qmusic_api_processor'
+      when 'Sublime FM'
+        rs.url = 'https://sublime.nl/sublime-playlist/'
+        rs.processor = 'scraper'
+      when 'Groot Nieuws Radio'
+        rs.url = 'https://www.grootnieuwsradio.nl/muziek/playlist'
+        rs.processor = 'scraper'
+      else
+        Rails.logger.info "Radio station #{rs.name} not found..."
+      end
+
+      rs.save!
+    end
+  end
+end
