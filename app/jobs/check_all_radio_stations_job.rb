980d044da35471a0e5fc1aec09f553ba2ccfb57b
Samuel Van Eck <samuelvaneck@adcombi.com>
2020-12-18 10:20:22 +0100

    Add import song to new sidekiq job

--- app/jobs/check_all_radio_stations_job.rb
+++ app/jobs/check_all_radio_stations_job.rb
@@ -5,12 +5,8 @@ class CheckAllRadioStationsJob < ApplicationJob
 
   def perform
     Radiostation.all.each do |radio_station|
-      puts "****** Import song #{radio_station.name} ******"
-      radio_station.import_song
+      ImportSongJob.perform_async(radio_station.id)
       sleep 10
-    rescue StandardError => e
-      puts "****** Error #{e} ******"
-      next
     end
   end
 end
---
+++ app/jobs/import_song_job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ImportSongJob
+  include Sidekiq::Worker
+
+  def perform(id)
+    radio_station = Radiostation.find(id)
+    puts "****** Import song #{radio_station.name} ******"
+    radio_station.import_song
+  rescue StandardError => e
+    puts "****** Error #{e} ******"
+  end
+end
