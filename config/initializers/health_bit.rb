bc8d74da4c32e663a5253d026f3efc96bbdd2155
Samuel Van Eck <samuelvaneck@adcombi.com>
2021-11-07 09:23:03 +0100

    Fix health checks

--- config/initializers/health_bit.rb
+++ config/initializers/health_bit.rb
@@ -2,7 +2,7 @@
 
 HealthBit.configure do |c|
   # DEFAULT SETTINGS ARE SHOWN BELOW
-  c.success_text = '%<count>d checks passed 🎉'
+  c.success_text = '%<count>d checks passed'
   c.headers = {
     'Content-Type' => 'text/plain;charset=utf-8',
     'Cache-Control' => 'private,max-age=0,must-revalidate,no-store'
@@ -10,21 +10,11 @@ HealthBit.configure do |c|
   c.success_code = 200
   c.fail_code = 500
   c.show_backtrace = false
-
-  c.add('Check name') do
-    # Body check, should returns `true`
-    true
-  end
 end
 
 # Database check
 HealthBit.add('Database') do
   ApplicationRecord.connection.select_value('SELECT 1') == 1
-end
-
-# Redis check
-HealthBit.add('Redis') do
-  Redis.current.ping == 'PONG'
 end
 
 # Sidekiq check
