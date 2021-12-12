62ec50ec5b5fd74617bd2805130ab9d4962be93e
Samuel Van Eck <samuelvaneck@adcombi.com>
2021-12-12 19:37:59 +0100

    Fix Sublime FM import

--- app/models/concerns/importable.rb
+++ app/models/concerns/importable.rb
@@ -96,8 +96,7 @@ module Importable
     when 'Sublime FM'
       last_hour = "#{date_string} #{Time.zone.now.hour}:00:00"
       next_hour = "#{date_string} #{Time.zone.now.hour == 23 ? '00' : Time.zone.now.hour + 1}:00:00"
-      data = `curl 'https://sublime.nl/wp-content/themes/OnAir2ChildTheme/phpincludes/sublime-playlist-query.php' \
-              -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8' \
+      data = `curl 'https://sublime.nl/wp-content/themes/OnAir2ChildTheme/phpincludes/sublime-playlist-query-api.php' \
               --data-raw 'request_from=#{last_hour}&request_to=#{next_hour}'`
 
       playlist = Nokogiri::HTML(data)
