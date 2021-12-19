0a72439d0f13944c2ddf920e4a74bc02850ef3e8
Samuel Van Eck <samuelvaneck@adcombi.com>
2021-12-19 16:46:55 +0100

    Fix Rubocop offences

--- spec/support/fake_spotify.rb
+++ spec/support/fake_spotify.rb
@@ -13,8 +13,8 @@ class FakeSpotify < Sinatra::Base
     content_type :json
     status 200
     body ({
-      'access_token': 'BQADenodY_gAhAusZHnMChTTCWsvNmoqTg-poCMY8p3fsws1HQkhTh0UEsr7fpsSEr1avKoju2RqP6REwd4',
-      'token_type': 'Bearer',
-      'expires_in': 3600 }).to_json
+      access_token: 'NgCXRKc...MzYjw',
+      token_type: 'Bearer',
+      expires_in: 3600 }).to_json
   end
 end
