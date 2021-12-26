17e0345f3c1fc214910495b49af02a06b3f4c88d
Samuel Van Eck <samuelvaneck@adcombi.com>
2021-12-26 14:32:17 +0100

    Visual improvement status bar

--- app/javascript/components/status_bar/status.vue
+++ app/javascript/components/status_bar/status.vue
@@ -1,11 +1,11 @@
 <template>
   <div v-if='warning'>
     <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" style='postion:absolute;display:none'>
-    <label class="btn btn-outline-danger mx-1" for="option1">{{ item.name }}</label>
+    <label class="status-button-warning" for="option1">{{ item.name }}</label>
   </div>
   <div v-else>
     <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" style='position:absolute;display:none'>
-    <label class="btn btn-outline-success mx-1" for="option1">{{ item.name }}</label>
+    <label class="status-button-ok" for="option1">{{ item.name }}</label>
   </div>
 </template>
 
@@ -18,7 +18,6 @@
       }
     },
     mounted: function() {
-      console.log(this.item.status)
       this.warning = this.item.status == 'warning'
     }
   }
