76d274358fae2c168b26d446ff199213d255556f
Samuel Van Eck <samuelvaneck@adcombi.com>
2021-12-29 15:22:35 +0100

    Add scheduled job to create chart

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -7,21 +7,19 @@ class Chart < ApplicationRecord
   scope :artists_charts, -> { where(chart_type: 'artists') }
 
   def self.create_yesterdays_charts
-    create_yesterday_chart('songs')
-    create_yesterday_chart('artists')
+    create_yesterdays_chart('songs')
+    create_yesterdays_chart('artists')
   end
 
-  def chart_postion(id, type)
-
+  def self.create_yesterdays_chart(chart_type)
+    chart = Chart.new(date: 1.day.ago.beginning_of_day, chart_type: chart_type)
+    chart.chart = chart.__send__("yesterdays_#{chart_type}_chart".to_sym)
+    chart.save
   end
+
+  def chart_postion(id, type); end
 
   private
-
-  def create_yesterday_chart(chart_type)
-    chart = Chart.new(date: 1.day.ago.beginning_of_day, chart_type: chart_type)
-    chart.chart = chart.send("yesterdays_#{chart_type}_chart".to_sym)
-    chart.save!
-  end
 
   def yesterdays_songs_chart
     songs = Song.search({ start_time: yesterday_beginning_of_day, end_time: yesterday_end_of_day })
@@ -33,13 +31,9 @@ class Chart < ApplicationRecord
     Artist.group_and_count(artists)
   end
 
-  def song_position(song_id)
+  def song_position(song_id); end
 
-  end
-
-  def artist_position(artist_id)
-
-  end
+  def artist_position(artist_id); end
 
   def yesterday_beginning_of_day
     1.day.ago.beginning_of_day.strftime('%FT%R')
--- spec/factories/charts.rb
+++ spec/factories/charts.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :chart do
-    date { "2021-12-29 12:47:49" }
-    chart { "" }
+    date { 1.day.ago }
+    chart { [] }
+    chart_type { ['songs', 'artists'].sample }
   end
 end
