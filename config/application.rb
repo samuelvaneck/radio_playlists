d742e94e27ee3e2b5f6059b925a287cad5b1b0cd
Samuel Van Eck <samuelvaneck@adcombi.com>
2022-01-20 13:41:49 +0100

    Gem update to Rails 7

--- config/application.rb
+++ config/application.rb
@@ -1,30 +1,22 @@
-# frozen_string_literal: true
+require_relative "boot"
 
-require_relative 'boot'
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
 module RadioPlaylists
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set the local time zone
-    config.time_zone = "Amsterdam"
-    config.active_record.default_timezone = :local
-
-    # Configure Sidekiq as default job handler
-    config.active_job.queue_adapter = :sidekiq
-    # Fix errors when running jobs with sidekiq
-    config.autoload_paths += %W[#{config.root}/app/jobs]
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
