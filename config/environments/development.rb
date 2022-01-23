f8f7c4bb9a451be4e6e5a2665030a8fdc4bab40d
Samuel Van Eck <samuelvaneck@adcombi.com>
2022-01-23 18:37:08 +0100

    Fix docker env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,4 +64,6 @@ Rails.application.configure do
   logger           = ActiveSupport::Logger.new(STDOUT)
   logger.formatter = config.log_formatter
   config.logger = ActiveSupport::TaggedLogging.new(logger)
+
+  config.hosts << '0.0.0.0'
 end
