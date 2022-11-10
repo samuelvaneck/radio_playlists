45350e15f5d00bc72c165f3f0c3e5f0fd8771fa1
Samuel Van Eck <samuelvaneck@adcombi.com>
2022-11-10 15:48:59 +0100

    Remove absolute tab

--- db/migrate/20221110141120_add_uniq_index_on_artists_songs.rb
+++ db/migrate/20221110141120_add_uniq_index_on_artists_songs.rb
@@ -1,7 +1,7 @@
 class AddUniqIndexOnArtistsSongs < ActiveRecord::Migration[7.0]
   def change
     Song.all.each do |song|
-        song.artists = song.artists.uniq
+      song.artists = song.artists.uniq
     end
 
     add_index :artists_songs, [:artist_id, :song_id], unique: true
