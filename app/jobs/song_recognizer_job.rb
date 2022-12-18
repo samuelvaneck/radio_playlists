259f88a6b6eb95b932848e8f49d3c4422bc79fe6
Samuel Van Eck <samuelvaneck@adcombi.com>
2022-12-18 17:30:39 +0100

    Code refactoring

--- app/jobs/song_recognizer_job.rb
+++ app/jobs/song_recognizer_job.rb
@@ -2,8 +2,6 @@ class SongRecognizerJob < ApplicationJob
   queue_as :default
 
   def perform
-    RadioStation.all.each do |radio_station|
-      radio_station.enqueue_recognize_song
-    end
+    RadioStation.all.each(&:enqueue_recognize_song)
   end
 end
