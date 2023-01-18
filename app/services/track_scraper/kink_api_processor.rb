912a9328908655c638194000ff8b1a362dee3cce
Samuel Van Eck <samuelvaneck@adcombi.com>
2023-01-18 20:40:53 +0100

    Fix kink api processor

--- app/services/track_scraper/kink_api_processor.rb
+++ app/services/track_scraper/kink_api_processor.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class TrackScrapper::KinkApiProcessor < TrackScraper
+class TrackScraper::KinkApiProcessor < TrackScraper
   def last_played_song
     uri = URI @radio_station.url
     json = JSON(make_request).with_indifferent_access
@@ -9,7 +9,7 @@ class TrackScrapper::KinkApiProcessor < TrackScraper
     track = json[:extended][:kink]
     @artist_name = track[:artist].titleize
     @title = track[:title].titleize
-    @broadcast_timestamp = Time.find_zone('Amsterdam').parse(Time.zone.now)
+    @broadcast_timestamp = Time.zone.now
     true
   rescue Net::ReadTimeout => _e
     Rails.logger.info "#{uri.host}:#{uri.port} is NOT reachable (ReadTimeout)"
