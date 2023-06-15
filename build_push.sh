85f14ef0ecc4242df90bb44e058c90e1a2c20568
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2023-06-15 07:34:55 +0200

    Refactor build script

--- build_push.sh
+++ build_push.sh
@@ -1,4 +1,3 @@
-docker build -t radio_web:latest .
 echo $CR_PAT | docker login ghcr.io --username samuelvaneck --password-stdin
-docker tag radio_web ghcr.io/samuelvaneck/radio_playlists:latest
+docker build -t ghcr.io/samuelvaneck/radio_playlists:latest .
 docker push ghcr.io/samuelvaneck/radio_playlists:latest
