2fbbb88fb4841934869ee5b56359fc00410a5a20
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2023-07-03 21:20:45 +0200

    Split artist and use isrc code for spoity lookup

--- app/services/spotify/track/search_url.rb
+++ app/services/spotify/track/search_url.rb
@@ -3,6 +3,8 @@ module Spotify
     class SearchUrl < Base
       attr_reader :title, :artists, :spotify_url, :isrc
 
+      FEATURING_REGEX = /\(feat\..+\)/.freeze
+
       def initialize(args)
         super
         @title = args[:title]
@@ -18,14 +20,42 @@ module Spotify
       private
 
       def search_params
-        return @spotify_url if @spotify_url.present?
+        # return @spotify_url if @spotify_url.present?
 
-        CGI.escape("#{@title} track:#{@title} artist:#{split_artists}")
+        params = if title_has_featuring_artists?
+                   "#{title_without_featuring_artists} track:#{title_without_featuring_artists} artist:#{split_artists_with_featuring_artists}"
+                 else
+                   "#{@title.downcase} track:#{@title.downcase} artist:#{split_artists}"
+                 end
+        params += " isrc:#{@isrc}" if @isrc.present?
+        params.gsub(" ", "%20")
       end
 
       def split_artists
         regex = Regexp.new(ENV['MULTIPLE_ARTIST_REGEX'], Regexp::IGNORECASE)
         @artists.match?(regex) ? @artists.downcase.split(regex).map(&:strip).join(' ') : @artists.downcase
+      end
+
+      def split_artists_with_featuring_artists
+        "#{split_artists} #{featuring_artists}".downcase
+      end
+
+      def featuring_artists
+        return @title unless title_has_featuring_artists?
+
+        regex = Regexp.new(FEATURING_REGEX)
+        title.scan(regex).map { |i| i.split("(feat.")[1] }[0].strip.gsub(')', '').downcase
+      end
+
+      def title_has_featuring_artists?
+        @title.match?(Regexp.new(FEATURING_REGEX))
+      end
+
+      def title_without_featuring_artists
+        return @title unless title_has_featuring_artists?
+
+        regex = Regexp.new(FEATURING_REGEX)
+        title.split(regex)[0].strip.downcase
       end
     end
   end
