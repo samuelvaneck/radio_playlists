3140875e23a0155f0584a4c4a00da37a8ce78849
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2023-07-06 21:55:50 +0200

    Fix featuring artists

--- app/services/spotify/track/search_url.rb
+++ app/services/spotify/track/search_url.rb
@@ -3,8 +3,6 @@ module Spotify
     class SearchUrl < Base
       attr_reader :title, :artists, :spotify_url, :isrc
 
-      FEATURING_REGEX = /\(feat\..+\)/
-
       def initialize(args)
         super
         @title = args[:title]
@@ -20,42 +18,14 @@ module Spotify
       private
 
       def search_params
-        params = if title_has_featuring_artists?
-                   "#{title_without_featuring_artists} track:#{title_without_featuring_artists} artist:#{split_artists_with_featuring_artists}"
-                 elsif @spotify_url.present?
-                   @spotify_url
-                 else
-                   "#{@title.downcase} track:#{@title.downcase} artist:#{split_artists}"
-                 end
-        params += " isrc:#{@isrc}" if @isrc.present? && @spotify_url.blank?
-        params.gsub(' ', '%20')
+        return @spotify_url.gsub('spotify:search:', '') if @spotify_url.present?
+
+        CGI.escape("#{@title} artist:#{split_artists}").gsub('+', '%20')
       end
 
       def split_artists
         regex = Regexp.new(ENV['MULTIPLE_ARTIST_REGEX'], Regexp::IGNORECASE)
         @artists.match?(regex) ? @artists.downcase.split(regex).map(&:strip).join(' ') : @artists.downcase
-      end
-
-      def split_artists_with_featuring_artists
-        "#{split_artists} #{featuring_artists}".downcase
-      end
-
-      def featuring_artists
-        return @title unless title_has_featuring_artists?
-
-        regex = Regexp.new(FEATURING_REGEX)
-        title.scan(regex).map { |i| i.split('(feat.')[1] }[0].strip.delete(')').downcase
-      end
-
-      def title_has_featuring_artists?
-        @title.match?(Regexp.new(FEATURING_REGEX))
-      end
-
-      def title_without_featuring_artists
-        return @title unless title_has_featuring_artists?
-
-        regex = Regexp.new(FEATURING_REGEX)
-        title.split(regex)[0].strip.downcase
       end
     end
   end
