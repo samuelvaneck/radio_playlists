99743db455d2c1396bf332a8674d6cfad006f60d
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2023-08-13 20:15:54 +0200

    Refactor cleanup job

--- app/jobs/clean_up_job.rb
+++ app/jobs/clean_up_job.rb
@@ -6,17 +6,35 @@ class CleanUpJob < ApplicationJob
   queue_as :default
 
   def perform
-    FileUtils.mkdir_p(Rails.root.join('tmp/recognizer_logs'))
-    file = Rails.root.join('tmp', 'recognizer_logs', "recognizer_logs_#{1.day.ago.strftime('%F')}.csv")
-    out_dated_logs = SongRecognizerLog.outdated
+    create_recognizer_folder
+    write_logs_to_csv_file
+    delete_outdated_logs
+  end
 
-    CSV.open(file, 'w') do |writer|
-      writer << out_dated_logs.first.attributes.map { |colunm_name, _value| colunm_name }
+  private
+
+  def create_recognizer_folder
+    FileUtils.mkdir_p(Rails.root.join('tmp/recognizer_logs'))
+  end
+
+  def write_logs_to_csv_file
+    CSV.open(csv_file, 'w') do |writer|
+      writer << out_dated_logs.first.attributes.map { |column_name, _value| column_name }
       out_dated_logs.each do |log|
-        writer << log.attributes.map { |_colunm_name, value| value }
+        writer << log.attributes.map { |_column_name, value| value }
       end
     end
+  end
 
+  def csv_file
+    Rails.root.join('tmp', 'recognizer_logs', "recognizer_logs_#{Time.now.strftime('%F')}.csv")
+  end
+
+  def delete_outdated_logs
     out_dated_logs.delete_all
+  end
+
+  def out_dated_logs
+    @out_dated_logs ||= SongRecognizerLog.outdated
   end
 end
