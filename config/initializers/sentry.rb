927e29b89d822244287665f8c91da11318690657
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2023-08-23 07:54:21 +0200

    Remove transaction and performance monitoring from Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,10 +6,6 @@ if Rails.env.production?
     # Set tracesSampleRate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production
-    config.traces_sample_rate = 0.5
-    # or
-    config.traces_sampler = lambda do |context|
-      true
-    end
+    # config.traces_sample_rate = 0.5
   end
 end
