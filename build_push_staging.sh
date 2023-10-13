28a1b18e4a70c0bbf913970cde4aee3219ebe936
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2023-10-13 08:20:15 +0200

    Build for multiple platforms

--- build_push_staging.sh
+++
@@ -1,3 +0,0 @@
-echo $CR_PAT | docker login ghcr.io --username samuelvaneck --password-stdin
-docker build -t ghcr.io/samuelvaneck/radio_playlists:arm-latest -f arm.Dockerfile .
-docker push ghcr.io/samuelvaneck/radio_playlists:arm-latest
