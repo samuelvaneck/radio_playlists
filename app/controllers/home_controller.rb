0135a0f4dfc4c46a876157c02f199025b3ec1ce5
Samuel van Eck <samuelvaneck@gmail.com>
2023-12-18 07:49:20 +0100

    Switch views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @songs = Song.most_played({}).limit(12)
     @radio_stations = RadioStation.all
+    render :index, params: { view: params[:view] }
   end
 end
