f002177ebf2d38f1139bf9565f7ef9443a37331e
Samuel van Eck <samuelvaneck@gmail.com>
2024-05-11 21:54:24 +0200

    Add ENV variables to set cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins *ENV.fetch('CORS_ORIGINS').split(" ").map(&:strip)
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
