2c40f9113583ac3a6534690d0326cdfb6abbd462
Samuel van Eck <samuelvaneck@gmail.com>
2024-05-15 22:07:28 +0200

    Fix setting counter on single artist

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -17,7 +17,11 @@
 class ArtistSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :id, :name, :spotify_artist_url, :spotify_artwork_url, :counter
+  attributes :id, :name, :spotify_artist_url, :spotify_artwork_url
+
+  attribute :counter do |object|
+    object.counter if object.respond_to?(:counter)
+  end
 
   has_many :songs
 end
