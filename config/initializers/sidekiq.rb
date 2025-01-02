258d0c3cc84ff49f7960b255419e62f1f7a9e46d
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2025-01-02 06:03:08 +0100

    Add Sidekiq unique jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,21 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_SIDEKIQ_URL'] }
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+
+  config.server_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Server
+  end
+
+  SidekiqUniqueJobs::Server.configure(config)
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_SIDEKIQ_URL'] }
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
 end
