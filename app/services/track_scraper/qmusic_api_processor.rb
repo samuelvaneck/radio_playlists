c7f8ee9e402be6bb63131163e669178919b8c74e
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2025-01-20 11:56:09 +0100

    Fix issue Qmusic scraper

--- app/services/track_scraper/qmusic_api_processor.rb
+++ app/services/track_scraper/qmusic_api_processor.rb
@@ -2,7 +2,7 @@
 
 class TrackScraper::QmusicApiProcessor < TrackScraper
   def last_played_song
-    json = JSON(make_request)
+    json = make_request
     return false if json.blank?
 
     track = json['played_tracks'][0]
@@ -11,6 +11,10 @@ class TrackScraper::QmusicApiProcessor < TrackScraper
     @title = track['title'].titleize
     @spotify_url = track['spotify_url']
     true
+  rescue StandardError => e
+    Rails.logger.error("Error in TrackScraper::QmusicApiProcessor: #{e.message}")
+    ExceptionNotifier.notify_new_relic(e)
+    nil
   end
 
   def make_request(url = @radio_station.url)
