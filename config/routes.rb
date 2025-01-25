2451a8b223febecaef67a693f1c6c08813e7fc5a
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2025-01-25 16:55:47 +0100

    Add Devise and admins model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ require 'sidekiq_unique_jobs/web'
 require 'sidekiq-scheduler/web'
 
 Rails.application.routes.draw do
+  devise_for :admins
   namespace :api do
     namespace :v1 do
       resources :playlists, only: %i[index show]
