23e9a7fffe6101ded0ebb43b4792c22cdf2babcc
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2025-07-06 16:37:07 +0200

    Set session id on token to make sure uniqness

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   before_action :authenticate_admin!
+  before_action :maybe_set_refresh_token
+
+
+  private
+  def maybe_set_refresh_token
+    return if current_admin.blank?
+    return if request.session_options[:skip]
+
+    session_id = request.session_options[:id]
+    return if request.session_options[:id].blank?
+
+    refresh_token = session[:refresh_token]
+    return if refresh_token.present?
+
+    session[:refresh_token] = RefreshToken.create(admin: current_admin, session_id:)
+  end
 end
--- db/migrate/20250608115152_create_refresh_tokens.rb
+++ db/migrate/20250608115152_create_refresh_tokens.rb
@@ -4,9 +4,11 @@ class CreateRefreshTokens < ActiveRecord::Migration[8.0]
       t.references :admin, null: false, foreign_key: true
       t.string :token, null: false
       t.datetime :expires_at
+      t.string :session_id, null: false
 
       t.timestamps
     end
     add_index :refresh_tokens, :token, unique: true
+    add_index :refresh_tokens, :session_id, unique: true
   end
 end
