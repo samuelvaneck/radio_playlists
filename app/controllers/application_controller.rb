2f90b5512444eae16791dfe1f075d9f26c6cebcd
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2025-07-06 21:56:28 +0200

    Add RSpec tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,26 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   before_action :authenticate_admin!
-  before_action :maybe_set_refresh_token
-
+  before_action :maybe_set_refresh_token?
 
   private
-  def maybe_set_refresh_token
+
+  def maybe_set_refresh_token?
     return if current_admin.blank?
     return if request.session_options[:skip]
 
     session_id = request.session_options[:id]
     return if request.session_options[:id].blank?
 
-    refresh_token = session[:refresh_token]
-    return if refresh_token.present?
+    current_refresh_token = session[:refresh_token]
+    if current_refresh_token.present? && current_refresh_token[:expires_at] > Time.zone.now
+      RefreshToken.find_by(token: current_refresh_token[:token], session_id:, admin: current_admin)&.destroy
+    elsif current_refresh_token.present? && current_refresh_token[:expires_at] < Time.zone.now
+      return
+    end
 
-    session[:refresh_token] = RefreshToken.create(admin: current_admin, session_id:)
+    new_token = RefreshToken.create(admin: current_admin, session_id:)
+    session[:refresh_token] = { token: new_token.token,
+                                expires_at: new_token.expires_at }
   end
 end
