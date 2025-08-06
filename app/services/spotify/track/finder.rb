fbb79c5ae727e537797592b72e865de61ba8f8e5
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2025-08-06 09:01:36 +0200

    Add support for release date percision month

--- app/services/spotify/track/finder.rb
+++ app/services/spotify/track/finder.rb
@@ -125,6 +125,8 @@ module Spotify
 
         if @track.dig('album', 'release_date_precision') != 'day'
           @track.dig('album', 'release_date')
+        elsif @track.dig('album', 'release_date_precision') == 'month'
+          "#{@track.dig('album', 'release_date')}-01"
         elsif @track.dig('album', 'release_date_precision') == 'year'
           "#{@track.dig('album', 'release_date')}-01-01"
         end
