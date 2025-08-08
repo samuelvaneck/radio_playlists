c7fd9a603b2c1133de4448b670834e27b33bf8a8
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2025-08-08 18:42:29 +0200

    Rename Spotify::ArtistFinder

--- app/jobs/radio_station_classifier_job.rb
+++ app/jobs/radio_station_classifier_job.rb
@@ -83,7 +83,7 @@ class RadioStationClassifierJob
   def track_artists_tags(args)
     track = ::Spotify::TrackFinder::FindById.new(id_on_spotify: args[:id_on_spotify]).execute
     track['artists'].flat_map do |artist|
-      spotify_artist = Spotify::Artist.new(id_on_spotify: artist['id']).info
+      spotify_artist = Spotify::ArtistFinder.new(id_on_spotify: artist['id']).info
       spotify_artist['genres']
     end.uniq
   end
