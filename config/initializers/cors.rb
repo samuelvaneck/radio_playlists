98c86d32ddee0ef9edf316613bad03d9342ffaf1
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2026-03-27 06:19:43 +0100

    Removed local cors allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://localhost:5173').split(',')
+    origins ENV.fetch('CORS_ALLOWED_ORIGINS').split(',')
 
     resource '*',
              headers: :any,
